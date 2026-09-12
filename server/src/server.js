91991d634c788a86c11b0d0d3fa9527066f165be
Tirth Patel <pateltirth1025@gmail.com>
2026-09-12 09:50:12 +0530

    initial setup done

---
+++ server/src/server.js
@@ -0,0 +1,8 @@
+import app from "./app.js";
+import dotenv from "dotenv";
+
+const PORT = process.env.PORT || 5000;
+dotenv.config();
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+})
